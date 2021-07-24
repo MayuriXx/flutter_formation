51621f7815ac2277b568bbea1cad283d1a31499c
MayuriXx <martho.evan@gmail.com>
2021-07-24 20:54:01 +0200

    feat(ux): add app with ux

---
+++ lib/model/Cours.dart
@@ -0,0 +1,8 @@
+class Cours {
+  final String? name;
+  final String? description;
+  final String? image;
+  final String? price;
+  final String? note;
+  Cours({this.name, this.description, this.image, this.price, this.note});
+}
---
+++ lib/pages/ListPage.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_formation/model/Cours.dart';
+import 'package:flutter_formation/pages/InfosPage.dart';
+
+final cours = List<Cours>.generate(
+  10,
+  (i) => Cours(
+    name: "Cours $i",
+    description:
+        "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée. Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsumn $i",
+    image: "assets/images/image_list_page.jpeg",
+    price: "29,9€",
+    note: "$i",
+  ),
+);
+
+class ListPage extends StatefulWidget {
+  ListPage({Key? key}) : super(key: key);
+
+  @override
+  _ListPageState createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Liste des formations"),
+        centerTitle: true,
+      ),
+      body: ListView.separated(
+          itemBuilder: (BuildContext context, int index) {
+            Cours crs = Cours(
+              name: cours[index].name,
+              description: cours[index].description,
+              image: cours[index].image,
+              price: cours[index].price,
+              note: cours[index].note,
+            );
+            return Container(
+              height: 70,
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => InfoPage(cours: crs),
+                    ),
+                  );
+                },
+                leading: Image.asset(crs.image ?? "/"),
+                title: Text(crs.name ?? ""),
+                subtitle: Row(
+                  children: [
+                    Text("${crs.price ?? ''} - ${crs.note ?? ''}"),
+                    Icon(
+                      Icons.star,
+                      size: 18.0,
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) => Divider(),
+          itemCount: cours.length),
+    );
+  }
+}
